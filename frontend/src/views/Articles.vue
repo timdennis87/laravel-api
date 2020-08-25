6b4ecb1b5c3d6fe2252ff652f77c4089e792ae29
Tim Dennis <timdennis1987@gmail.com>
2020-08-25 12:14:24 +0100

    creating simple single page vue application

---
+++ frontend/src/views/Articles.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <div v-for="item in articles" :key="item.id">
+            <h1>{{item.title}}</h1>
+            <p>{{item.body}}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    name: 'Articles',
+    data() {
+        return {
+            articles: null
+        }
+    },
+    created() {
+        this.getArticles()
+    },
+    methods: {
+        getArticles() {
+            axios
+                .get('/api/articles')
+                .then(res => {
+                    const {data:{data}} = res
+                    this.articles = data
+                    console.log(res)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
